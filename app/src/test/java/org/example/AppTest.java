4d3dffcc11e528c5c2c9b2541693c486b7673ab6
fidannazarova <fnazarova15216@ada.edu.az>
2026-02-02 02:25:08 +0400

    Initial commit: Gradle Java project setup

---
+++ app/src/test/java/org/example/AppTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void appRuns() {
+        // simple test that always passes
+        assert true;
+    }
+}
